321487f6d4fb7ad393dad5e9fde995a6ea8f8ab3
Dbm713 <duybui71103@gmail.com>
2024-11-06 23:05:40 +0700

    update domains deploy be

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'ia-04-be-production.up.railway.app';
+const API_URL = 'https://ia-04-be-production.up.railway.app';
 
 export const register = (email: string, password: string) => {
     return axios.post(
